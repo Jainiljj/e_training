1a1e83eef2c4864f9fd2a0878b4151a57f3fa4c3
Jainiljj <jainilsinghvi18@gmail.com>
2026-07-20 23:48:08 +0530

    just showing up...

--- react/first_react_project/src/App.jsx
+++ react/first_react_project/src/App.jsx
@@ -1,18 +1,1 @@
 import React from "react";
-
-const App = () => {
-  const display = (value) => {
-   console.log(value);
-   console.log("Button Clicked");
-};
-return(
-  <div>
-    <h1>
-      Events
-    </h1>
-    <button onClick={() => display("Data send")}>Click me</button>
-  </div>
-);
-};
-
-export default App;
